a338168e7453d5e9d2e3569532c3aa98e70eb859
darrellbanksdb-dev <darrell.banks.db@gmail.com>
2026-03-02 15:31:46 -0600

    Update master_table_build.sql

--- sql/master_table_build.sql
+++ sql/master_table_build.sql
@@ -1,1 +1,19 @@
+#master_table_build.sql
+CREATE OR REPLACE TABLE `dbvision.Bell_Beat.bellabeats_daily_master` AS
+SELECT
+  a.user_id,
+  a.activity_date,
+  a.total_steps,
+  a.calories,
+  a.very_active_minutes,
+  a.fairly_active_minutes,
+  a.lightly_active_minutes,
+  a.sedentary_minutes,
+  s.minutes_asleep,
+  s.minutes_in_bed,
+  SAFE_DIVIDE(s.minutes_asleep, s.minutes_in_bed) AS sleep_efficiency
+FROM `dbvision.Bell_Beat.daily_activity_clean` a
+LEFT JOIN `dbvision.Bell_Beat.sleep_day_clean` s
+  ON a.user_id = s.user_id
+ AND a.activity_date = s.sleep_date;
 
